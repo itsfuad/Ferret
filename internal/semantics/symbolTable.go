c566176b14cb9b265eac2a13a3c903eaf5ef0a4d
Fuad Hasan <fuad.cs22@gmail.com>
2025-12-04 21:23:22 +0600

    follow up

--- internal/semantics/symbolTable.go
+++
@@ -1,9 +0,0 @@
-package semantics
-
-// import "compiler/internal/semantics/symbols"
-
-// type SymbolTable interface {
-// 	Declare(name string, symbol *symbols.Symbol) error
-// 	Lookup(name string) (*symbols.Symbol, bool)
-// 	GetSymbol(name string) (*symbols.Symbol, bool)
-// }
