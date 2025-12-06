ec1322fb3b6de89eae763ce857338bea72e52511
Fuad Hasan <fuad.cs22@gmail.com>
2025-12-07 00:47:27 +0600

    feat: Update Ferret WASM version to 0.0.5-alpha and refresh binary files

--- main_wasm.go
+++ main_wasm.go
@@ -10,7 +10,7 @@ import (
 
 func main() {
 	js.Global().Set("ferretCompile", js.FuncOf(compile))
-	js.Global().Set("ferretWasmVersion", "0.0.4")
+	js.Global().Set("ferretWasmVersion", "0.0.5-alpha")
 	println("Ferret WASM compiler ready")
 	<-make(chan struct{})
 }
